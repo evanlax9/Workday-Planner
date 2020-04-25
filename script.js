b804abeb512810643c7b78e0f73637fb5890e82d
Evan <62257610+evanlax9@users.noreply.github.com>
2020-04-25 15:59:32 -0400

    Almost Done

--- script.js
+++ script.js
@@ -2,10 +2,55 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
   $(".saveBtn").on("click", function () {
     var plansValue = $(this).siblings(".description").val();
-    localStorage.setItem("name", plansValue);
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, plansValue);
     console.log(plansValue);
   });
+  var currentHour = moment().hour();
+
+  function hourUpdate() {
+    $(".time-block").each(function () {
+      var newHour = parseInt($(this).attr("id").split("-")[0]);
+      if (currentHour > newHour) {
+        $(this).addClass("past");
+
+
+      }
+      else if (currentHour === newHour) {
+        $(this).addClass("present")
+        $(this).removeClass("past")
+      }
+
+      else if (currentHour < newHour) {
+        $(this).addClass("future");
+        $(this).removeClass("present")
+        $(this).removeClass("past")
+
+
+      }
+
+
+    })
+  }
+  hourUpdate();
+
+  $("#9-Am .description").val(localStorage.getItem("9-Am"));
+  $("#10-Am .description").val(localStorage.getItem("10-Am"));
+  $("#11-Am .description").val(localStorage.getItem("11-Am"));
+  $("#12-Pm .description").val(localStorage.getItem("12-Pm"));
+  $("#13-Pm .description").val(localStorage.getItem("13-Pm"));
+  $("#14-Pm .description").val(localStorage.getItem("14-Pm"));
+  $("#15-Pm .description").val(localStorage.getItem("15-Pm"));
+  $("#16-Pm .description").val(localStorage.getItem("16-Pm"));
+  $("#17-Pm .description").val(localStorage.getItem("17-Pm"));
+
+  if (currentHour > 15) {
+    localStorage.clear();
+  }
+
+
 });
+
 
 // Use moment to get today's date
 // format should be [DAY OF WEEK], [Full Month] [DAYOFMONTH ex 3rd]
